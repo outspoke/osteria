7cbd6cfd5a088626e9daa8daf7df0a7282438f3d
Jay Nielsen <jayarnielsen@gmail.com>
2018-03-15 12:43:11 -0600

    remove css nano

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ module.exports = ({ file, options }) => {
   return {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
-      cssnano: options.enabled.optimize ? cssnanoConfig : false,
+      // cssnano: options.enabled.optimize ? cssnanoConfig : false,
       autoprefixer: true,
     },
   };
